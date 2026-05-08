6b7d50e87e2afbe48deb3d7af6810621710432e4
almstkshfuae <almstkshfuae@gmail.com>
2026-05-08 12:11:26 +0400

    feat: update Noto Sans Arabic font configuration for improved performance

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,8 @@ const notoSansArabic = Noto_Sans_Arabic({
   subsets: ['arabic'],
   weight: ['400', '500', '600', '700'],
   variable: '--font-noto-sans-arabic',
+  display: 'swap',
+  preload: false,
 });
 
 
